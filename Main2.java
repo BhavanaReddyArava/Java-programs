13f7f11bc74722224f3a55a29a0413ab7a16083c
BhavanaReddyArava <aravabhavana2004@gmail.com>
2025-06-03 15:14:36 +0530

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+public class Main2
+{
+	public static void main(String[] args) {
+	    Stack s=new Stack<>();
+	    s.push(2);
+	    s.push(3);
+		System.out.println(s);
+		System.out.println(s.pop());
+		System.out.println(s.peek());
+	}
+}
